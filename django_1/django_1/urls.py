1b871dcad90f0bc2cdcea6aa62f3d0d2b8eb6684
JulioG07 <juliogoitia12@gmail.com>
2025-04-21 20:39:45 -0400

    Program outputs 'hello world' through /playground/hello/

--- django_1/django_1/urls.py
+++ django_1/django_1/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #playground/hello - line 23 says that if path is "playground" it lets playground app handle it
+    path('playground/', include('playground.urls'))
 ]
+
